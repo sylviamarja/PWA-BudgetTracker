92e0407f5babd0d34397f45a0b758c3fda8efc47
Sarah Marie Azzopardi <sarahazzopardi17@hotmail.com>
2021-09-15 17:40:16 +0200

    fixing code

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://azzopars:ZoaLp24qGfokNx9s@cluster0.7rzs3.mongodb.net/socialapi?retryWrites=true&w=majority';
 
 const app = express();
 
